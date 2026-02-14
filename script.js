403ac5daa65d8fb0c46ee79014e11fc9c2d3e780
kendekdue2548 <kendekdue2548@gmail.com>
2026-02-15 01:19:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function checkPasscode() {
+  const code = document.getElementById('passcode').value;
+  if (code === '123456') { // ตั้งรหัสที่ต้องการ
+    document.getElementById('login-page').style.display = 'none';
+    document.getElementById('main-menu').style.display = 'block';
+  } else {
+    alert('รหัสผ่านไม่ถูกต้องนะจ๊ะ');
+  }
+}
